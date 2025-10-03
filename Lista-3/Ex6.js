ecc4f494ed0d73f21de827094f4da11e396e3739
Magnetic | Matheus Oliveira <123500110+matheusBatistaDeOliveira@users.noreply.github.com>
2025-10-02 23:52:04 -0300

    Update Ex6.js

--- Lista-3/Ex6.js
+++ Lista-3/Ex6.js
@@ -1,4 +1,4 @@
-import input from 'readline-sync'
+const input = require('readline-sync')
 let num1 = input.questionInt("Digite o 1° número: ")
 let num2 = input.questionInt("Digite o 2° número: ")
 
