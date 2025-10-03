9e917e8dec878379e90396ffc9cbd25b881bb401
Magnetic | Matheus Oliveira <123500110+matheusBatistaDeOliveira@users.noreply.github.com>
2025-10-03 02:12:44 -0300

    Update ex2.js

--- lista-4/ex2.js
+++ lista-4/ex2.js
@@ -1,7 +1,8 @@
 let input = require("readline-sync")
-let num1 = input.questionInt('digite um numero e mostre os numeros entre ele e 20');
-num1 = num1 + 1
-while (num1<=19) {
-    console.log(num1);
+let num1 = input.questionInt('Digite um número e mostre os números entre ele e 20:')
 
+num1 = num1 + 1
+while (num1 < 20) {
+    console.log(num1)
+    num1 = num1 + 1
 }
